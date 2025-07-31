253260141d080ab67073145b200cfddc4a7baef4
최겸준/인사서비스개발팀/ND <jun1718@nhndooray.com>
2025-07-31 19:23:23 +0900

    Feat : aws api 서버로 url변경

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.API_BASE_URL || 'http://localhost:8080',
+  baseURL: process.env.API_BASE_URL || 'https://h-infinite-power.store',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
